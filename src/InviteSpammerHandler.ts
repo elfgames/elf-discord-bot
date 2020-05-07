4a717c3087bdcb2af9b116cee4b10a9ce06115de
ProGM <progiemmeh@gmail.com>
2020-05-07 23:19:34 +0200

    :new: Kick the user after multiple spam

--- src/InviteSpammerHandler.ts
+++ src/InviteSpammerHandler.ts
@@ -2,14 +2,26 @@ import { Message } from 'discord.js';
 import { IMessageHandler } from './IMessageHandler';
 
 export default class InviteSpammerHandler implements IMessageHandler {
+  spammers: { [key: string]: number }
+
   canHandle(message: Message) {
     return message.content.indexOf('//discord.gg/') !== -1 && !message.member.hasPermission('MANAGE_CHANNELS');
   }
 
   async handle(message: Message) {
-    // await message.member.kick('Don\'t spam your discord channel without permission.');
+    if (this.spammers[message.member.id]) {
+      this.spammers[message.member.id]++;
+    } else {
+      this.spammers[message.member.id] = 0;
+    }
+
+    if (this.spammers[message.member.id] > 0) {
+      await message.member.kick('NEVER spam your discord channel.');
+      console.log(`Kicked user ${message.member.user.username}, id: ${message.member.id}`)
+    }
 
     await message.delete();
-    await message.channel.send(`${message.member.nickname || message.member.user.username}: Please don't spam other discord channels.`)
+    await message.channel.send(`@${message.member.user.username}: Please don't spam other discord channels.`)
+    console.log(`Deleted spam message from ${message}: ${message.member.user.username}`);
   }
 }
