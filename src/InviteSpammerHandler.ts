99c6da3494e6193a7042b5152e8a51e6177ac6f2
ProGM <progiemmeh@gmail.com>
2020-05-07 23:40:17 +0200

    :fix: struct null checks

--- src/InviteSpammerHandler.ts
+++ src/InviteSpammerHandler.ts
@@ -5,23 +5,28 @@ export default class InviteSpammerHandler implements IMessageHandler {
   spammers: { [key: string]: number } = {};
 
   canHandle(message: Message) {
-    return message.content.indexOf('//discord.gg/') !== -1 && !message.member.hasPermission('MANAGE_CHANNELS');
+    return message.content.indexOf('//discord.gg/') !== -1 && !message.member?.hasPermission('MANAGE_CHANNELS');
   }
 
   async handle(message: Message) {
-    if (this.spammers[message.member.id]) {
-      this.spammers[message.member.id]++;
-    } else {
-      this.spammers[message.member.id] = 0;
-    }
+    const user = message.member;
+    const username = message.member?.user?.username || 'someone';
 
-    if (this.spammers[message.member.id] > 0) {
-      await message.member.kick('NEVER spam your discord channel.');
-      console.log(`Kicked user ${message.member.user.username}, id: ${message.member.id}`)
+    if (user) {
+      if (this.spammers[user.id]) {
+        this.spammers[user.id]++;
+      } else {
+        this.spammers[user.id] = 0;
+      }
+
+      if (this.spammers[user.id] > 0) {
+        await user.kick('NEVER spam your discord channel.');
+        console.log(`Kicked user ${username}, id: ${user.id}`)
+      }
     }
 
     await message.delete();
-    await message.channel.send(`@${message.member.user.username}: Please don't spam other discord channels.`)
-    console.log(`Deleted spam message from ${message}: ${message.member.user.username}`);
+    await message.channel.send(`@${username}: Please don't spam other discord channels.`)
+    console.log(`Deleted spam message from ${message}: ${username}`);
   }
 }
